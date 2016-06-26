ae7de2eb9aaf17e7ac52eb1999c27dae2a19e675
Ben <lyenliang@gmail.com>
2016-06-26 14:58:49 +0000

    crawl stock data work in progress

--- app/services/crawl_tdcc_data_service.rb
+++ app/services/crawl_tdcc_data_service.rb
@@ -4,11 +4,28 @@ class CrawlTdccDataService
     
     require 'open-uri'
     
+
     def initialize
     end
     
     def fetch_all_data
+        targetURL = "http://www.tdcc.com.tw/smWeb/QryStock.jsp"
         all_stocks = fetch_all_stock_number
+        all_stocks.each do |stock|
+            url = targetURL + '?SCA_DATE=' + '20160617' + '&SqlMethod=StockNo&StockNo=' + stock + '&StockName=&sub=%ACd%B8%DF'
+            
+            web_data = Nokogiri::HTML(open(url))
+            byebug
+            # 
+            # tables[6] contains 證券代號
+            # tables[7] 
+            tables = web_data.css("table")
+            
+            title_text = tables[6].css("tr").css("td")[0].inner_text
+            stock_number = /\d+/.match(title_text).to_s
+            stock_name = title_text.split(/\s/)[1].split("：")[1]
+            
+        end
     end
     
     private 
@@ -21,7 +38,7 @@ class CrawlTdccDataService
         list = text.split(/\r?\n/)
         list.each do |l|
             if /^\d{4}/.match(l)
-                stock_list << l
+                stock_list << l.squish
             end
         end
         return stock_list
