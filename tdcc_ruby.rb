7abf09456ac4e29d6a215fa8304a28c5e8b41834
Ben <lyenliang@gmail.com>
2016-07-09 08:53:17 +0000

    Add type

--- tdcc_ruby.rb
+++ tdcc_ruby.rb
@@ -15,7 +15,7 @@ require 'nokogiri'
 @otc_price = "http://www.tpex.org.tw/web/stock/aftertrading/daily_trading_info/st43_download.php"
 @exchange_market_price = "http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY_AVG/STOCK_DAY_AVG2.php"
 
-@client = Mysql2::Client.new(:host => @db_host, :username => @db_user)
+@client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass)
 @client.query("USE #{@db_name}")
 
 def init()
@@ -39,7 +39,7 @@ end
 
 def reset_price_table
     @client.query("DROP TABLE IF EXISTS #{@stock_price_table_name};")
-    @client.query("CREATE TABLE #{@stock_price_table_name} (stock_number VARCHAR(255), date DATE, closing_price FLOAT);")
+    @client.query("CREATE TABLE #{@stock_price_table_name} (stock_number VARCHAR(255), date DATE, closing_price FLOAT, type VARCHAR(255));")
 end
 
 def reset_db
@@ -49,13 +49,17 @@ def reset_db
 end
 
 def insert_tdcc_table(snumber, sname, date, sgroup, people, ss, pt)
-    @client.query("INSERT INTO #{@tdcc_table_name} (stock_number, stock_name, date, share_group, people, shares, percent) 
+    @client.query("INSERT INTO #{@tdcc_table_name} (stock_number, stock_name, date, share_group, people, shares, percent)
             VALUES ('#{snumber}', '#{sname}', '#{date}', '#{sgroup}', '#{people}', '#{ss}', '#{pt}');")
 end
 
-def insert_price_table(stock_number, date, closing_price)
-    @client.query("INSERT INTO #{@stock_price_table_name} (stock_number, date, closing_price)
-            VALUES ('#{stock_number}', '#{date}', '#{closing_price}');")
+def insert_price_table(stock_number, date, closing_price, type)
+    puts "stock_number: #{stock_number}"
+    puts "date: #{date}"
+    puts "closing_price: #{closing_price}"
+    puts "type: #{type}"
+    @client.query("INSERT INTO #{@stock_price_table_name} (stock_number, date, closing_price, type)
+            VALUES ('#{stock_number}', '#{date}', '#{closing_price}', '#{type}');")
 end
 
 def fetch_tdcc_all_data(dates, stocks)
@@ -73,26 +77,26 @@ def fetch_tdcc_single_date(stock, date)
         url = @tdcc_url + '?SCA_DATE=' + date + '&SqlMethod=StockNo&StockNo=' + stock + '&StockName=&sub=%ACd%B8%DF'
         puts "stock: #{stock}"
         puts "date: #{date}"
-        
+
         open_url = open(url)
         web_data = Nokogiri::HTML(open_url)
-        
-        # 
+
+        #
         # tables[6] contains 證券代號
-        # tables[7] 
+        # tables[7]
         tables = web_data.css("table")
-        
+
         title_text = tables[6].css("tr").css("td")[0].inner_text
         stock_number = /\d+/.match(title_text).to_s
         stock_name = title_text.split(/\s/)[1].split("：")[1]
-        
+
         rows = tables[7].css("tbody").css("tr")
-        
+
         for i in 1..15
             cells = rows[i].css("td")
             insert_tdcc_table(stock_number, stock_name, date, cells[1].inner_text, cells[2].inner_text.gsub(/,/, ''), cells[3].inner_text.gsub(/,/, ''), cells[4].inner_text)
         end
-    
+
     rescue Exception => e
         # continue the loop
         puts "Error!!!"
@@ -117,15 +121,15 @@ def fetch_all_stock_number
 end
 
 def fetch_all_dates
-    # return a list of all the dates 
+    # return a list of all the dates
     dates = []
     open_url = open(@tdcc_url)
     date_tmp = Nokogiri::HTML(open_url).css("option")
-    
+
     date_tmp.each do |date_t|
         dates << date_t.inner_text
     end
-    
+
     return dates
 end
 
@@ -141,7 +145,7 @@ def truncate_old_dates(last_date, all_dates)
     if index > 0
         length = index + 1
         return all_dates[0, length]
-    else 
+    else
         return all_dates[0,0]
     end
 end
@@ -151,12 +155,12 @@ def fetch_new_data
     all_dates = fetch_all_dates
     new_dates = truncate_old_dates(last_date, all_dates)
     all_stocks = fetch_all_stock_number
-    
+
     all_stocks.each do |stock|
         new_dates.each do |date|
             fetch_tdcc_single_date(stock, date)
         end
-    end    
+    end
 end
 
 def fetch_price_all_data(dates, stocks)
@@ -174,10 +178,10 @@ end
 def extractMonths(dates)
     last = dates[0]
     first = dates[-1]
-    
+
     first_month = first[4...6].to_i
     first_year = ad_to_tw(first[0, 4].to_i)
-    
+
     last_month = last[4...6].to_i
     last_year = ad_to_tw(last[0, 4].to_i)
     return calculateWholeMonths(first_year, first_month, last_year, last_month)
@@ -190,7 +194,7 @@ def calculateWholeMonths(first_year, first_month, last_year, last_month)
         leading_zero_m = "#{m}".rjust(2, '0')
         allMonths << "#{last_year}/#{leading_zero_m}"
     end
-        
+
     for m in (12).downto(first_month)
         leading_zero_m = "#{m}".rjust(2, '0')
         allMonths << "#{first_year}/#{leading_zero_m}"
@@ -215,12 +219,12 @@ def fetch_price(stkno, date)
         puts e.backtrace.inspect
     end
     # 上市
-    
+
 end
 
 def fetch_stock_exchange(stock, date)
     # 上市
-    # example: 
+    # example:
     # http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY_AVG/STOCK_DAY_AVG2.php?STK_NO=2303&myear=2016&mmon=02&type=csv
     puts "Fetch stock exchange #{stock}, #{date}"
     year_month = date.split('/')
@@ -238,21 +242,20 @@ def fetch_stock_exchange(stock, date)
         end
         date = date_price[0]
         date = transform_date(date)
-        
-        insert_price_table(stock, date, closing_price)
+        insert_price_table(stock, date, closing_price, 'exchange')
     end
 end
 
 # OTC: over-the-counter
 def fetch_otc(stock, date)
     # 上櫃
-    # example: 
+    # example:
     # http://www.tpex.org.tw/web/stock/aftertrading/daily_trading_info/st43_download.php?l=zh-tw&d=105/07&stkno=3662&s=0,asc,0
     # stock = "3662"
     url = @otc_price + '?l=zh-tw&d=' + date + '&stkno=' + stock + '&s=0,asc,0'
     puts "stock: #{stock}"
     puts "date: #{date}"
-    
+
     open_url = open(url)
     web_data = Nokogiri::HTML(open_url)
     rows = web_data.inner_text.split(/\r?\n/)
@@ -263,9 +266,9 @@ def fetch_otc(stock, date)
         row_data = rows[i].split(/,/)
         date = row_data[0]
         date = transform_date(date)
-        # transform date to 20XX-XX-XX 
+        # transform date to 20XX-XX-XX
         price = row_data[7].gsub(/"/, '')
-        insert_price_table(stock, date, price)
+        insert_price_table(stock, date, price, 'otc')
     end
     return true
 end
